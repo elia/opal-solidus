bf715cb52624c7db7387c2c7b4acd248e76adbff
Elia Schito <elia@schito.me>
2023-04-22 11:59:20 +0200

    Enable source-maps in the Stimulus stacktrace

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,35 @@
 import { Application } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
+import StackTrace from 'stacktrace-js'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = true
+window.Stimulus = application
+window.Stimulus.Controller = Controller
+
+// Stimulus.handleError = null
+Stimulus.handleError = (error, message, detail) => {
+  StackTrace.fromError(error)
+    .then((stack) => {
+      stack = stack.filter((s) => s.fileName && !s.fileName.match(/\.min[-\.]/))
+      stack.forEach((s) => {
+        if (s.fileName.match(/\.rb$/)) {
+          const url = new URL(location.href)
+          url.pathname = `/assets/opal/${s.fileName}`
+          s.fileName = url.toString()
+        }
+      })
+      console.error(
+        `Stimulus error: ${message}`,
+        detail,
+        `\n\n${error}\n\n${stack.join('\n')}\n\n`,
+      )
+    })
+    .catch((_e) => {
+      throw error
+    })
+}
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,9 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "opal/application", to: "opal/application.js", preload: true
+pin "stacktrace-js", to: "https://ga.jspm.io/npm:stacktrace-js@2.0.2/stacktrace.js"
+pin "error-stack-parser", to: "https://ga.jspm.io/npm:error-stack-parser@2.1.4/error-stack-parser.js"
+pin "source-map/lib/source-map-consumer", to: "https://ga.jspm.io/npm:source-map@0.5.6/lib/source-map-consumer.js"
+pin "stack-generator", to: "https://ga.jspm.io/npm:stack-generator@2.0.10/stack-generator.js"
+pin "stackframe", to: "https://ga.jspm.io/npm:stackframe@1.3.4/stackframe.js"
+pin "stacktrace-gps", to: "https://ga.jspm.io/npm:stacktrace-gps@3.1.2/stacktrace-gps.js"
